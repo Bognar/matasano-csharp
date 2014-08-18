4a480f0bc388fecd400f6967393c8db901747c4e
Chad Kimes <ckimes89@gmail.com>
2014-08-18 02:29:29 +0000

    Set 2 challenge 1

---
+++ Matasano/Set2/Challenge1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matasano.Set2
+{
+    public class Challenge1
+    {
+        public static byte[] PKCS7Pad(byte[] input, int blockSize)
+        {
+            var mod = input.Length % blockSize;
+            var padCount = blockSize - mod;
+            var outLength = input.Length + (mod == 0 ? 0 : blockSize - mod);
+            var output = new byte[outLength];
+            Array.Copy(input, output, input.Length);
+
+            for(var i = input.Length; i < output.Length; i++)
+            {
+                output[i] = (byte)padCount;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ MatasanoTests/Set2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatasanoTests
+{
+    [TestClass]
+    public class Set2
+    {
+        [TestMethod]
+        public void Challenge1()
+        {
+            var bytes = Encoding.ASCII.GetBytes("YELLOW SUBMARINE");
+            var padded = Matasano.Set2.Challenge1.PKCS7Pad(bytes, 20);
+            var paddedText = Encoding.ASCII.GetString(padded);
+            Assert.AreEqual("YELLOW SUBMARINE\x04\x04\x04\x04", paddedText);
+
+            var noPadding = Matasano.Set2.Challenge1.PKCS7Pad(bytes, 16);
+            CollectionAssert.AreEqual(noPadding, bytes);
+        }
+    }
+}
